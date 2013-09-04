877918cd629dfe197c1bc1f916add33207fe25dd
Moisés Rangel <mrangel@univermilenium>
2013-09-04 16:00:39 -0500

    SQL para dummies de documentos

---
+++ scripts/tosql.py
@@ -0,0 +1,13 @@
+import glob
+
+def getfiles(pattern):
+    return glob.glob(pattern)
+
+def printlistfiles(pattern):
+    files = getfiles(pattern)
+    for file in files:
+        filename = file.split("/")[-1];
+        print "INSERT INTO documentos (id_lic, grado, materia, clave, autor, ruta) VALUES (1, 'GRADO', 'PEDAGOGIA', 'CLAVEDPEDAGOGIA', 'autor', '/pedagogia/%s');" % filename
+
+if __name__ == "__main__":
+    printlistfiles("/home/moises/apps/repos/antologias/files/pedagogia/*.txt")
